c72a16cfeeecc3b5f5b8d166f3f5bbca5a5d9da2
tokwanku <92240226+tokwanku@users.noreply.github.com>
2023-03-08 15:28:27 +0800

    Add files via upload

---
+++ BMI/index.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://unicons.iconscout.com/release/v2.1.9/css/unicons.css">
+  <link rel="stylesheet" href="style4.css">
+  <title>Login</title>
+</head>
+
+<style>
+ body{
+  background-image: linear-gradient(120deg,#ff6b6b, #5f27cd);
+ }
+
+</style>
+
+<body>
+  <div class="section">
+    <div class="container">
+      <div class="row full-height justify-content-center">
+        <div class="col-12 text-center align-self-center py-5">
+          <div class="section pb-5 pt-5 pt-sm-2 text-center">
+            <h6 class="mb-0 pb-3"><span>Log In </span></h6>
+            
+            <div class="card-3d-wrap mx-auto">
+              <div class="card-3d-wrapper">
+                <div class="card-front">
+                  <div class="center-wrap">
+                    <div class="section text-center">
+
+                      <!-- ============================================================================================================= -->
+                      <!-- ============================================================================================================= -->
+                      <!-- ======================================================login section========================================== -->
+                      <!-- ============================================================================================================= -->
+                      <!-- ============================================================================================================= -->
+                      <!-- ============================================================================================================= -->
+
+                      <h4 class="mb-4 pb-3">Log In</h4>
+                      <form action="" method="post">
+                        <div class="form-group">
+                          <input type="text" name="text" class="form-style" placeholder="Username" id=""
+                            autocomplete="off" >
+                          <i class="input-icon uil uil-at"></i>
+                        </div>
+                        <div class="form-group mt-2">
+                          <input type="password" name="logpass" class="form-style" placeholder="Password"
+                            id="logpass" autocomplete="off" required>
+                          <i class="input-icon uil uil-lock-alt"></i>
+                        </div>
+                        <button type="submit" name="submit" class="btn mt-4"><a href="index.html">submit</a></button>
+                        
+                      </form>
+                    </div>
+                  </div>
+                </div>
+
+
+                <!-- ============================================================================================================= -->
+                <!-- ============================================================================================================= -->
+                <!-- ============================================================================================================= -->
+                <!-- ======================================================signup section========================================== -->
+                <!-- ============================================================================================================= -->
+                <!-- ============================================================================================================= -->
+                <!-- ============================================================================================================= -->
+
+
+                
+
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</body>
+
+</html>
